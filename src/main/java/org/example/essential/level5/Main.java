a5b9d3a05041f9224ce865cdb229307e1aab5ecc
Sungho <nanumeran@gmail.com>
2025-07-18 19:29:50 +0900

    Delete src/main/java/org/example/essential/level5 directory

--- src/main/java/org/example/essential/level5/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example.essential.level5;
-
-// 시작 지점이 되는 클래스
-public class Main {
-    public static void main(String[] args) {
-        Kiosk kiosk = new Kiosk();
-        kiosk.startKiosk();
-    }
-}
